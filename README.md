9d2b83f57cc48355b5d03a2a5eb54e48dede2f41
kunal-ch <kunalch36@gmail.com>
2022-11-13 23:40:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StockApp
+Stock app is an android app that displays mock holdings using Jetpack compose and Hilt based on Modern Android tech stacks
---
+++ app/src/main/java/com/kc/stockapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kc.stockapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import com.kc.stockapp.theme.StockAppTheme
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("Android")
+                }
+            }
+        }
+    }
+}
