02f7762e80a8c6577154cbfe743f8350a97ceea8
kunal-ch <kunalch36@gmail.com>
2022-11-15 12:40:55 +0530

    feat : Dagger Hilt

---
+++ app/src/main/java/com/kc/stockapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.kc.stockapp.di
+
+import com.kc.stockapp.data.remote.StockApi
+import com.kc.stockapp.utils.Constant.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }).build())
+            .build()
+            .create(StockApi::class.java)
+    }
+}
